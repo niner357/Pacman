6cfb1b15bdf291b1cbe6b87f4470ed7ffbd968ef
Rage Skilln <rageskilln@gmail.com>
2017-05-13 23:38:34 +0200

    Adding the First MySQL Basics

---
+++ Pacman/Pacman/LoginForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Diagnostics;
+using Pacman.MySQL;
+
+namespace Pacman
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://dasdarki.de/pacman/register.php");
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
